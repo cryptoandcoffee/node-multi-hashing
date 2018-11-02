c46e9bad7596c96252ea284c210ffbf16564dc11
globaltoken <admin@globaltoken.org>
2018-11-02 18:26:59 +0100

    Update gltalgos.c
    - Updated padihash

--- gltalgos.c
+++ gltalgos.c
@@ -320,113 +320,113 @@ void padihash(const char* input, char* output, uint32_t len)
     sph_shabal512_context    ctx_shabal;
 
     //these uint512 in the c++ source of the client are backed by an array of uint32
-    uint32_t hashA[16], hashB[16];	
+    uint32_t hash[16];	
 
     sph_sha512_init(&ctx_sha2);
     sph_sha512(&ctx_sha2, input, len);
-    sph_sha512_close(&ctx_sha2, hashA);
+    sph_sha512_close(&ctx_sha2, hash);
 
     sph_jh512_init(&ctx_jh);
-    sph_jh512(&ctx_jh, hashA, 64);
-    sph_jh512_close(&ctx_jh, hashB);
+    sph_jh512(&ctx_jh, hash, 64);
+    sph_jh512_close(&ctx_jh, hash);
     
     sph_luffa512_init(&ctx_luffa);
-    sph_luffa512(&ctx_luffa, hashB, 64);
-    sph_luffa512_close(&ctx_luffa, hashA);
+    sph_luffa512(&ctx_luffa, hash, 64);
+    sph_luffa512_close(&ctx_luffa, hash);
     
     sph_echo512_init(&ctx_echo);
-    sph_echo512(&ctx_echo, hashA, 64);
-    sph_echo512_close(&ctx_echo, hashB);
+    sph_echo512(&ctx_echo, hash, 64);
+    sph_echo512_close(&ctx_echo, hash);
     
     sph_bmw512_init(&ctx_bmw);
-    sph_bmw512(&ctx_bmw, hashB, 64);
-    sph_bmw512_close(&ctx_bmw, hashA);
+    sph_bmw512(&ctx_bmw, hash, 64);
+    sph_bmw512_close(&ctx_bmw, hash);
     
     sph_haval256_5_init(&ctx_haval);
-    sph_haval256_5(&ctx_haval, hashA, 64);
-    sph_haval256_5_close(&ctx_haval, hashB);
+    sph_haval256_5(&ctx_haval, hash, 64);
+    sph_haval256_5_close(&ctx_haval, hash);
     
     sph_cubehash512_init(&ctx_cubehash);
-    sph_cubehash512(&ctx_cubehash, hashB, 64);
-    sph_cubehash512_close(&ctx_cubehash, hashA);
+    sph_cubehash512(&ctx_cubehash, hash, 64);
+    sph_cubehash512_close(&ctx_cubehash, hash);
     
     sph_shabal512_init(&ctx_shabal);
-    sph_shabal512(&ctx_shabal, hashA, 64);
-    sph_shabal512_close(&ctx_shabal, hashB);
+    sph_shabal512(&ctx_shabal, hash, 64);
+    sph_shabal512_close(&ctx_shabal, hash);
     
     sph_sha512_init(&ctx_sha2);
-    sph_sha512(&ctx_sha2, hashB, 64);
-    sph_sha512_close(&ctx_sha2, hashA);
+    sph_sha512(&ctx_sha2, hash, 64);
+    sph_sha512_close(&ctx_sha2, hash);
 
     sph_jh512_init(&ctx_jh);
-    sph_jh512(&ctx_jh, hashA, 64);
-    sph_jh512_close(&ctx_jh, hashB);
+    sph_jh512(&ctx_jh, hash, 64);
+    sph_jh512_close(&ctx_jh, hash);
     
     sph_luffa512_init(&ctx_luffa);
-    sph_luffa512(&ctx_luffa, hashB, 64);
-    sph_luffa512_close(&ctx_luffa, hashA);
+    sph_luffa512(&ctx_luffa, hash, 64);
+    sph_luffa512_close(&ctx_luffa, hash);
     
     sph_echo512_init(&ctx_echo);
-    sph_echo512(&ctx_echo, hashA, 64);
-    sph_echo512_close(&ctx_echo, hashB);
+    sph_echo512(&ctx_echo, hash, 64);
+    sph_echo512_close(&ctx_echo, hash);
     
     sph_bmw512_init(&ctx_bmw);
-    sph_bmw512(&ctx_bmw, hashB, 64);
-    sph_bmw512_close(&ctx_bmw, hashA);
+    sph_bmw512(&ctx_bmw, hash, 64);
+    sph_bmw512_close(&ctx_bmw, hash);
     
     sph_haval256_5_init(&ctx_haval);
-    sph_haval256_5(&ctx_haval, hashA, 64);
-    sph_haval256_5_close(&ctx_haval, hashB);
+    sph_haval256_5(&ctx_haval, hash, 64);
+    sph_haval256_5_close(&ctx_haval, hash);
     
     sph_cubehash512_init(&ctx_cubehash);
-    sph_cubehash512(&ctx_cubehash, hashB, 64);
-    sph_cubehash512_close(&ctx_cubehash, hashA);
+    sph_cubehash512(&ctx_cubehash, hash, 64);
+    sph_cubehash512_close(&ctx_cubehash, hash);
     
     sph_shabal512_init(&ctx_shabal);
-    sph_shabal512(&ctx_shabal, hashA, 64);
-    sph_shabal512_close(&ctx_shabal, hashB);
+    sph_shabal512(&ctx_shabal, hash, 64);
+    sph_shabal512_close(&ctx_shabal, hash);
     
     sph_shabal512_init(&ctx_shabal);
-    sph_shabal512(&ctx_shabal, hashB, 64);
-    sph_shabal512_close(&ctx_shabal, hashA);
+    sph_shabal512(&ctx_shabal, hash, 64);
+    sph_shabal512_close(&ctx_shabal, hash);
     
     sph_cubehash512_init(&ctx_cubehash);
-    sph_cubehash512(&ctx_cubehash, hashA, 64);
-    sph_cubehash512_close(&ctx_cubehash, hashB);
+    sph_cubehash512(&ctx_cubehash, hash, 64);
+    sph_cubehash512_close(&ctx_cubehash, hash);
     
     sph_haval256_5_init(&ctx_haval);
-    sph_haval256_5(&ctx_haval, hashB, 64);
-    sph_haval256_5_close(&ctx_haval, hashA);
+    sph_haval256_5(&ctx_haval, hash, 64);
+    sph_haval256_5_close(&ctx_haval, hash);
     
     sph_bmw512_init(&ctx_bmw);
-    sph_bmw512(&ctx_bmw, hashA, 64);
-    sph_bmw512_close(&ctx_bmw, hashB);
+    sph_bmw512(&ctx_bmw, hash, 64);
+    sph_bmw512_close(&ctx_bmw, hash);
     
     sph_echo512_init(&ctx_echo);
-    sph_echo512(&ctx_echo, hashB, 64);
-    sph_echo512_close(&ctx_echo, hashA);
+    sph_echo512(&ctx_echo, hash, 64);
+    sph_echo512_close(&ctx_echo, hash);
     
     sph_luffa512_init(&ctx_luffa);
-    sph_luffa512(&ctx_luffa, hashA, 64);
-    sph_luffa512_close(&ctx_luffa, hashB);
+    sph_luffa512(&ctx_luffa, hash, 64);
+    sph_luffa512_close(&ctx_luffa, hash);
     
     sph_jh512_init(&ctx_jh);
-    sph_jh512(&ctx_jh, hashB, 64);
-    sph_jh512_close(&ctx_jh, hashA);
+    sph_jh512(&ctx_jh, hash, 64);
+    sph_jh512_close(&ctx_jh, hash);
     
     sph_sha512_init(&ctx_sha2);
-    sph_sha512(&ctx_sha2, hashA, 64);
-    sph_sha512_close(&ctx_sha2, hashB);
+    sph_sha512(&ctx_sha2, hash, 64);
+    sph_sha512_close(&ctx_sha2, hash);
     
     sph_jh512_init(&ctx_jh);
-    sph_jh512(&ctx_jh, hashB, 64);
-    sph_jh512_close(&ctx_jh, hashA);
+    sph_jh512(&ctx_jh, hash, 64);
+    sph_jh512_close(&ctx_jh, hash);
     
     sph_bmw512_init(&ctx_bmw);
-    sph_bmw512(&ctx_bmw, hashA, 64);
-    sph_bmw512_close(&ctx_bmw, hashB);
+    sph_bmw512(&ctx_bmw, hash, 64);
+    sph_bmw512_close(&ctx_bmw, hash);
 
-    memcpy(output, hashB, 32);
+    memcpy(output, hash, 32);
 }
 
 void globalhash(const char* input, char* output, uint32_t len)
