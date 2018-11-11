1ff01f64905d333470edd049d02cc30007f99d53
globaltoken <admin@globaltoken.org>
2018-11-11 23:22:58 +0100

    Revert "Update gltalgos.c"
    This reverts commit 02d67105587fd2956527b9a7ca29ab6df7a8ee61.

--- gltalgos.c
+++ gltalgos.c
@@ -272,58 +272,58 @@ void astralhash(const char* input, char* output, uint32_t len)
     uint32_t hashA[16], hashB[16];	
 
     sph_luffa512_init(&ctx_luffa);
-    sph_luffa512(&ctx_luffa, (const void*)&input, len);
-    sph_luffa512_close(&ctx_luffa, (void*)&hashA);
+    sph_luffa512(&ctx_luffa, input, len);
+    sph_luffa512_close(&ctx_luffa, hashA);
 
     sph_skein512_init(&ctx_skein);
-    sph_skein512(&ctx_skein, (const void*)&hashA, 64);
-    sph_skein512_close(&ctx_skein, (void*)&hashB);
+    sph_skein512(&ctx_skein, hashA, 64);
+    sph_skein512_close(&ctx_skein, hashB);
     
     sph_echo512_init(&ctx_echo);
-    sph_echo512(&ctx_echo, (const void*)&hashB, 64);
-    sph_echo512_close(&ctx_echo, (void*)&hashA);
+    sph_echo512(&ctx_echo, hashB, 64);
+    sph_echo512_close(&ctx_echo, hashA);
     
     sph_whirlpool_init(&ctx_whirlpool);
-    sph_whirlpool(&ctx_whirlpool, (const void*)&hashA, 64);
-    sph_whirlpool_close(&ctx_whirlpool, (void*)&hashB);
+    sph_whirlpool(&ctx_whirlpool, hashA, 64);
+    sph_whirlpool_close(&ctx_whirlpool, hashB);
     
     sph_bmw512_init(&ctx_bmw);
-    sph_bmw512(&ctx_bmw, (const void*)&hashB), 64);
-    sph_bmw512_close(&ctx_bmw, (void*)&hashA);
+    sph_bmw512(&ctx_bmw, hashB, 64);
+    sph_bmw512_close(&ctx_bmw, hashA);
     
     sph_blake512_init(&ctx_blake);
-    sph_blake512(&ctx_blake, (const void*)&hashA, 64);
-    sph_blake512_close(&ctx_blake, (void*)&hashB);
+    sph_blake512(&ctx_blake, hashA, 64);
+    sph_blake512_close(&ctx_blake, hashB);
     
     sph_shavite512_init(&ctx_shavite);
-    sph_shavite512(&ctx_shavite, (const void*)&hashB, 64);
-    sph_shavite512_close(&ctx_shavite, (void*)&hashA);
+    sph_shavite512(&ctx_shavite, hashB, 64);
+    sph_shavite512_close(&ctx_shavite, hashA);
     
     sph_skein512_init(&ctx_skein);
-    sph_skein512(&ctx_skein, (const void*)&hashA, 64);
-    sph_skein512_close(&ctx_skein, (void*)&hashB);
+    sph_skein512(&ctx_skein, hashA, 64);
+    sph_skein512_close(&ctx_skein, hashB);
     
     sph_whirlpool_init(&ctx_whirlpool);
-    sph_whirlpool(&ctx_whirlpool, (const void*)&hashB, 64);
-    sph_whirlpool_close(&ctx_whirlpool, (void*)&hashA);
+    sph_whirlpool(&ctx_whirlpool, hashB, 64);
+    sph_whirlpool_close(&ctx_whirlpool, hashA);
     
     sph_fugue512_init(&ctx_fugue);
-    sph_fugue512(&ctx_fugue, (const void*)&hashA, 64);
-    sph_fugue512_close(&ctx_fugue, (void*)&hashB);
+    sph_fugue512(&ctx_fugue, hashA, 64);
+    sph_fugue512_close(&ctx_fugue, hashB);
     
     sph_hamsi512_init(&ctx_hamsi);
-    sph_hamsi512(&ctx_hamsi, (const void*)&hashB, 64);
-    sph_hamsi512_close(&ctx_hamsi, (void*)&hashA);
+    sph_hamsi512(&ctx_hamsi, hashB, 64);
+    sph_hamsi512_close(&ctx_hamsi, hashA);
     
     sph_haval256_5_init(&ctx_haval);
-    sph_haval256_5(&ctx_haval, (const void*)&hashA, 64);
-    sph_haval256_5_close(&ctx_haval, (void*)&hashB);
+    sph_haval256_5(&ctx_haval, hashA, 64);
+    sph_haval256_5_close(&ctx_haval, hashB);
     
     sph_sha512_init(&ctx_sha2);
-    sph_sha512(&ctx_sha2, (const void*)&hashB, 64);
-    sph_sha512_close(&ctx_sha2, (void*)&hashA);
+    sph_sha512(&ctx_sha2, hashB, 64);
+    sph_sha512_close(&ctx_sha2, hashA);
 
-    memcpy(output, &hashA, 32);
+    memcpy(output, hashA, 32);
     
     int ii = 0;
     
