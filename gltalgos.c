a9ec24e404392f15a6bd85d1dfd62bfc20a4f97c
globaltoken <admin@globaltoken.org>
2018-11-11 23:36:54 +0100

    Update gltalgos.c
    - Try it with char instead of uint

--- gltalgos.c
+++ gltalgos.c
@@ -42,86 +42,86 @@ void pawelhash(const char* input, char* output, uint32_t len)
     sph_bmw512_context       ctx_bmw;
     sph_gost512_context      ctx_gost;
     
-    //these uint512 in the c++ source of the client are backed by an array of uint32
-    uint32_t hashA[16], hashB[16];	
+    char hashA[64];
+    char hashB[64];	
 
     sph_fugue512_init(&ctx_fugue);
     sph_fugue512(&ctx_fugue, input, len);
-    sph_fugue512_close(&ctx_fugue, hashA);
+    sph_fugue512_close(&ctx_fugue, &hashA);
 
     sph_sha512_init(&ctx_sha2);
     sph_sha512(&ctx_sha2, hashA, 64);
-    sph_sha512_close(&ctx_sha2, hashB);
+    sph_sha512_close(&ctx_sha2, &hashB);
     
     sph_skein512_init(&ctx_skein);
     sph_skein512(&ctx_skein, hashB, 64);
-    sph_skein512_close(&ctx_skein, hashA);
+    sph_skein512_close(&ctx_skein, &hashA);
 
     sph_jh512_init(&ctx_jh);
     sph_jh512(&ctx_jh, hashA, 64);
-    sph_jh512_close(&ctx_jh, hashB);
+    sph_jh512_close(&ctx_jh, &hashB);
 
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512(&ctx_keccak, hashB, 64);
-    sph_keccak512_close(&ctx_keccak, hashA);
+    sph_keccak512_close(&ctx_keccak, &hashA);
     
     sph_luffa512_init(&ctx_luffa);
     sph_luffa512(&ctx_luffa, hashA, 64);
-    sph_luffa512_close(&ctx_luffa, hashB);
+    sph_luffa512_close(&ctx_luffa, &hashB);
     
     sph_whirlpool_init(&ctx_whirlpool);
     sph_whirlpool(&ctx_whirlpool, hashB, 64);
-    sph_whirlpool_close(&ctx_whirlpool, hashA);
+    sph_whirlpool_close(&ctx_whirlpool, &hashA);
     
     sph_shabal512_init(&ctx_shabal);
     sph_shabal512(&ctx_shabal, hashA, 64);
-    sph_shabal512_close(&ctx_shabal, hashB);
+    sph_shabal512_close(&ctx_shabal, &hashB);
     
     sph_echo512_init(&ctx_echo);
     sph_echo512(&ctx_echo, hashB, 64);
-    sph_echo512_close(&ctx_echo, hashA);
+    sph_echo512_close(&ctx_echo, &hashA);
     
     sph_groestl512_init(&ctx_groestl);
     sph_groestl512(&ctx_groestl, hashA, 64);
-    sph_groestl512_close(&ctx_groestl, hashB);
+    sph_groestl512_close(&ctx_groestl, &hashB);
     
     sph_haval256_5_init(&ctx_haval);
     sph_haval256_5(&ctx_haval, hashB, 64);
-    sph_haval256_5_close(&ctx_haval, hashA);
+    sph_haval256_5_close(&ctx_haval, &hashA);
     
     sph_bmw512_init(&ctx_bmw);
     sph_bmw512(&ctx_bmw, hashA, 64);
-    sph_bmw512_close(&ctx_bmw, hashB);
+    sph_bmw512_close(&ctx_bmw, &hashB);
     
     sph_echo512_init(&ctx_echo);
     sph_echo512(&ctx_echo, hashB, 64);
-    sph_echo512_close(&ctx_echo, hashA);
+    sph_echo512_close(&ctx_echo, &hashA);
     
     sph_fugue512_init(&ctx_fugue);
     sph_fugue512(&ctx_fugue, hashA, 64);
-    sph_fugue512_close(&ctx_fugue, hashB);
+    sph_fugue512_close(&ctx_fugue, &hashB);
     
     sph_bmw512_init(&ctx_bmw);
     sph_bmw512(&ctx_bmw, hashB, 64);
-    sph_bmw512_close(&ctx_bmw, hashA);
+    sph_bmw512_close(&ctx_bmw, &hashA);
 
     sph_gost512_init(&ctx_gost);
     sph_gost512(&ctx_gost, hashA, 64);
-    sph_gost512_close(&ctx_gost, hashB);
+    sph_gost512_close(&ctx_gost, &hashB);
     
     sph_shabal512_init(&ctx_shabal);
     sph_shabal512(&ctx_shabal, hashB, 64);
-    sph_shabal512_close(&ctx_shabal, hashA);
+    sph_shabal512_close(&ctx_shabal, &hashA);
     
     sph_whirlpool_init(&ctx_whirlpool);
     sph_whirlpool(&ctx_whirlpool, hashA, 64);
-    sph_whirlpool_close(&ctx_whirlpool, hashB);
+    sph_whirlpool_close(&ctx_whirlpool, &hashB);
     
     sph_groestl512_init(&ctx_groestl);
     sph_groestl512 (&ctx_groestl, hashB, 64);
-    sph_groestl512_close(&ctx_groestl, hashA);
+    sph_groestl512_close(&ctx_groestl, &hashA);
 
-    memcpy(output, hashA, 32);
+    memcpy(output, &hashA, 32);
     
     int ii = 0;
     
